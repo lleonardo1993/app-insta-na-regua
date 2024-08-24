2b25e07b98cf9a2fcb181389a33060f9fb4e7f00
leonardo <lleonardo1555@gmail.com>
2024-08-24 15:49:19 -0300

    feat: component primary

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { DefaultLoginLayoutComponent } from '../../default-login-layout/default-login-layout.component';
 import { FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { PrimaryInputComponent } from '../../components/primary-input/primary-input.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [
     DefaultLoginLayoutComponent,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    PrimaryInputComponent
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
