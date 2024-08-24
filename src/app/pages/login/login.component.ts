edc4984f012b69e29f8724965bcc6c8fb112a282
leonardo <lleonardo1555@gmail.com>
2024-08-24 14:26:18 -0300

    feat: default login layout

---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { DefaultLoginLayoutComponent } from '../../default-login-layout/default-login-layout.component';
+import { FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [
+    DefaultLoginLayoutComponent,
+    ReactiveFormsModule
+  ],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+  loginForm!: FormGroup
+
+  constructor(){
+    this.loginForm = new FormGroup({
+      email: new FormGroup('', [Validators.required, Validators.email]),
+      password: new FormGroup('', [Validators.required, Validators.minLength(6)])
+    })
+  }
+}
